f3613422216b1d9cdb0d662d18079712584f9b9c
Wesley Moy <wesley.moy@gmail.com>
2015-12-17 22:38:43 -0800

    Empty root response handler

--- rsvp.go
+++ rsvp.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 )
 
+func Root(w http.ResponseWriter, r *http.Request) {
+}
+
 func main() {
+	http.HandleFunc("/", Root)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- rsvp_test.go
+++ rsvp_test.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRoot(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := &http.Request{}
+	Root(w, r)
+}
