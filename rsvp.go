21c17d11b775a5f78976f450d1309841880c1175
Wesley Moy <wesley.moy@gmail.com>
2015-12-18 07:37:09 -0800

    Connect to empty database

--- rsvp.go
+++ rsvp.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"fmt"
+
+	_ "github.com/lib/pq"
 )
 
 func Root(w http.ResponseWriter, r *http.Request) {
@@ -17,6 +22,12 @@ func Root(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	p := os.Getenv("POSTGRES_ENV_POSTGRES_PASSWORD")
+	s := fmt.Sprintf("user=postgres dbname=postgres password=%s sslmode=verify-full", p)
+	_, err := sql.Open("postgres", s)
+	if err != nil {
+		log.Fatal(err)
+	}
 	http.HandleFunc("/", Root)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
